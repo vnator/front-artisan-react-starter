041cebbbf67b24ab50c533cac0a2817bfb0c2a1b
Joao Henrique Serodio Ulbinski <joao.ulbinski@iesde.com.br>
2019-12-16 17:34:26 -0300

    feat (Users): add users style

---
+++ src/components/Icon/IconFlatLeft.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Type from 'prop-types';
+import { useIntl } from 'react-intl';
+
+import { Svg, viewBoxType } from './Svg';
+
+const IconFlatLeft = props => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <Svg {...props}>
+      <title>{formatMessage({ id: 'icon.flatLeft' })}</title>
+      <path d="M11.429 8v16c0 0.625-0.518 1.143-1.143 1.143-0.304 0-0.589-0.125-0.804-0.339l-8-8c-0.214-0.214-0.339-0.5-0.339-0.804s0.125-0.589 0.339-0.804l8-8c0.214-0.214 0.5-0.339 0.804-0.339 0.625 0 1.143 0.518 1.143 1.143z"></path>
+    </Svg>
+  );
+};
+
+IconFlatLeft.defaultProps = {
+  viewBox: {
+    minX: 0,
+    minY: 0,
+    width: 13,
+    height: 32,
+  },
+  size: {
+    height: undefined,
+    width: undefined,
+  },
+  className: '',
+};
+
+IconFlatLeft.propTypes = {
+  size: Type.shape({
+    height: Type.number,
+    width: Type.number,
+  }),
+  viewBox: viewBoxType,
+  className: Type.string,
+};
+
+export { IconFlatLeft };
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -3,6 +3,7 @@ import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 import { useIntl } from 'react-intl';
 import style from './Users.module.css';
+import { IconFlatDown, IconFlatUp, IconFlatLeft, IconFlatRight } from '../../components/Icon';
 
 const query = gql`
   {
@@ -22,20 +23,42 @@ const Users = () => {
   console.log(data);
   return (
     <div className={style.Users}>
-      <h2>
+      <h2 className={style.title}>
         {formatMessage({
           id: 'users.title',
         })}
       </h2>
 
-      <ul>
+      <ul className={style.list}>
+        <li className={style.head}>
+          <button className={style.iconButton}>
+            name
+            <IconFlatDown className={style.icon} />
+          </button>
+          <button className={style.iconButton}>
+            email
+            <IconFlatUp className={style.icon} />
+          </button>
+        </li>
+
         {data &&
           data.users.map(user => (
-            <li key={user.id}>
-              <strong>{user.name} :</strong>
-              <span>{user.name}</span> - <span>{user.dateOfBirth}</span>
+            <li key={user.id} className={style.item}>
+              <a className={style.link}>
+                <strong className={style.cell}>{user.name}</strong>
+                <span className={style.cell}>{user.email}</span>
+              </a>
             </li>
           ))}
+        <li className={style.foot}>
+          <button className={style.iconButton}>
+            <IconFlatLeft className={style.icon} />
+          </button>
+          <strong className={style.page}>1</strong>
+          <button className={style.iconButton}>
+            <IconFlatRight className={style.icon} />
+          </button>
+        </li>
       </ul>
     </div>
   );
