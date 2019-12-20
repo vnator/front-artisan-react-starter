31b8610fe11c48114feb0e6e50c66c4afaf96fd3
Joao Henrique Serodio Ulbinski <joao.ulbinski@iesde.com.br>
2019-12-20 15:43:44 -0300

    test (components/Toast): add tests

--- src/components/Toast/__tests__/toast.test.js
+++ src/components/Toast/__tests__/toast.test.js
@@ -1,16 +1,98 @@
-const FEAT = 'Toast';
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { spy } from 'sinon';
 
-describe(`${FEAT}`, () => {
-  it(`${FEAT} Snapshot`, () => {
-    expect(false).toBeTruthy();
+import style from '../Toast.module.css';
+import { Toast } from '../Toast';
+
+Enzyme.configure({ adapter: new Adapter() });
+const mockStore = configureMockStore([thunk]);
+
+describe('Toast', () => {
+  it('Snapshot', () => {
+    const store = mockStore({
+      toast: {
+        message: '',
+        active: false,
+      },
+    });
+
+    const toast = mount(
+      <Provider store={store}>
+        <Toast />
+      </Provider>,
+    );
+
+    expect(toast).toMatchSnapshot();
   });
-  it(`${FEAT} message`, () => {
-    expect(false).toBeTruthy();
+
+  it('message', () => {
+    const toast = {
+      message: 'any message',
+      active: false,
+    };
+
+    const store = mockStore({
+      toast,
+    });
+
+    const toastComponent = mount(
+      <Provider store={store}>
+        <Toast />
+      </Provider>,
+    );
+
+    const dom = toastComponent.find('div').prop('children');
+
+    expect(dom).toBe(toast.message);
   });
-  it(`${FEAT} active`, () => {
-    expect(false).toBeTruthy();
+
+  it('active', () => {
+    const toast = {
+      message: 'any message',
+      active: true,
+    };
+
+    const store = mockStore({
+      toast,
+    });
+
+    const toastComponent = mount(
+      <Provider store={store}>
+        <Toast />
+      </Provider>,
+    );
+
+    const dom = toastComponent.exists(`div.${style.Toast}.${style._active}`);
+
+    expect(dom).toBeTruthy();
   });
-  it(`${FEAT} auto hide`, () => {
-    expect(false).toBeTruthy();
+
+  it('auto hide', done => {
+    const toast = {
+      message: 'any message',
+      active: true,
+    };
+
+    const store = mockStore({
+      toast,
+    });
+
+    store.dispatch = spy();
+
+    const toastComponent = mount(
+      <Provider store={store}>
+        <Toast />
+      </Provider>,
+    );
+
+    setTimeout(() => {
+      expect(store.dispatch.calledOnce).toBeTruthy();
+      done();
+    }, 3000);
   });
 });
