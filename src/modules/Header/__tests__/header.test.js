d50c0c4ef8b9244a69d9e7bc85e64c67e5eb0ae3
Joao Henrique Serodio Ulbinski <joao.ulbinski@iesde.com.br>
2019-12-20 16:06:28 -0300

    test (modules/Heaer): add snapshot

--- src/modules/Header/__tests__/header.test.js
+++ src/modules/Header/__tests__/header.test.js
@@ -1,7 +1,15 @@
-const FEAT = 'Header';
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Header } from '../Header';
+import { mountWithIntl } from '../../../utils/enzymeHelper';
 
-describe(`${FEAT}`, () => {
-  it(`${FEAT} Snapshot`, () => {
-    expect(false).toBeTruthy();
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Header', () => {
+  it('Snapshot', () => {
+    const header = mountWithIntl(<Header />);
+
+    expect(header).toMatchSnapshot();
   });
 });
