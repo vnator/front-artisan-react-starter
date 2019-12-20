167f35afe55cad6d95492ef677cfa3070d9ea1e4
Joao Henrique Serodio Ulbinski <joao.ulbinski@iesde.com.br>
2019-12-20 16:32:17 -0300

    test (components/Toast/reducer): add tests

--- src/components/Toast/__tests__/reducer.test.js
+++ src/components/Toast/__tests__/reducer.test.js
@@ -1,11 +1,25 @@
-const FEAT = 'Toast Reducer';
+import { toast } from '../redux/types';
+import { toastReducer, _initialState } from '../redux/reducer';
 
-describe(`${FEAT}`, () => {
-  it(`${FEAT} setMessage`, () => {
-    expect(false).toBeTruthy();
+describe('Toast Reducer', () => {
+  it('default state', () => {
+    expect(toastReducer(undefined, {})).toEqual(_initialState);
   });
 
-  it(`${FEAT} toggleActive`, () => {
-    expect(false).toBeTruthy();
+  it('setMessage', () => {
+    const actionCreate = {
+      type: toast.SET_MESSAGE,
+      payload: 'message',
+    };
+
+    expect(toastReducer(actionCreate, {})).toEqual(actionCreate);
+  });
+
+  it('toggleActive', () => {
+    const actionCreate = {
+      type: toast.TOGGLE_ACTIVE,
+    };
+
+    expect(toastReducer(actionCreate, {})).toEqual(actionCreate);
   });
 });
